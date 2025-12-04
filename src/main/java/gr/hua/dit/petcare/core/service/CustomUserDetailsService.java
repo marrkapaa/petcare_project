882198ee3badcdf7bb5ac7075bbeea6ea1fec266
marrkapaa <151930230+marrkapaa@users.noreply.github.com>
2025-12-04 21:27:08 +0200

    Update CustomUserDetailsService.java

--- src/main/java/gr/hua/dit/petcare/core/service/CustomUserDetailsService.java
+++ src/main/java/gr/hua/dit/petcare/core/service/CustomUserDetailsService.java
@@ -2,20 +2,20 @@ package gr.hua.dit.petcare.core.service;
 
 import gr.hua.dit.petcare.core.model.User;
 import gr.hua.dit.petcare.core.repositories.UserRepository;
-
-import org.springframework.security.core.GrantedAuthority;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.Collections;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    private static final Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
     private final UserRepository userRepository;
 
     public CustomUserDetailsService(UserRepository userRepository) {
@@ -24,17 +24,24 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // βρίσκουμε τον χρήστη από το Repository
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+                .orElseThrow(() -> {
+                    logger.error("Login Failed: User not found with username: {}", username);
+                    return new UsernameNotFoundException("User not found with username: " + username);
+                });
 
-        Collection<? extends GrantedAuthority> authorities =
-                Collections.singleton(new SimpleGrantedAuthority(user.getRole().name()));
+        logger.info("User found: {}. Role: {}", user.getUsername(), user.getRole());
+
+
+        String roleName = user.getRole().name();
+        if (!roleName.startsWith("ROLE_")) {
+            roleName = "ROLE_" + roleName;
+        }
 
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
                 user.getPassword(),
-                authorities // στέλνουμε Collection<GrantedAuthority>
+                Collections.singletonList(new SimpleGrantedAuthority(roleName))
         );
     }
 }
