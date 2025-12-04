7eaf54db5c19aae9e7e5d762f5b31e2a6aa84cff
marrkapaa <151930230+marrkapaa@users.noreply.github.com>
2025-12-04 19:53:14 +0200

    Update CustomUserDetailsService.java

--- src/main/java/gr/hua/dit/petcare/core/service/CustomUserDetailsService.java
+++ src/main/java/gr/hua/dit/petcare/core/service/CustomUserDetailsService.java
@@ -24,19 +24,17 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // Βρίσκουμε τον χρήστη από το Repository
+        // βρίσκουμε τον χρήστη από το Repository
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
 
-        // 1. Δημιουργούμε το GrantedAuthority από το όνομα του ρόλου (π.χ. "OWNER")
         Collection<? extends GrantedAuthority> authorities =
                 Collections.singleton(new SimpleGrantedAuthority(user.getRole().name()));
 
-        // 2. Επιστρέφουμε το αντικείμενο UserDetails με το σωστό τύπο Authorities
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
-                user.getPassword(), // Ο κωδικός που είναι ήδη κρυπτογραφημένος
-                authorities // Τώρα στέλνουμε Collection<GrantedAuthority>
+                user.getPassword(),
+                authorities // στέλνουμε Collection<GrantedAuthority>
         );
     }
 }
