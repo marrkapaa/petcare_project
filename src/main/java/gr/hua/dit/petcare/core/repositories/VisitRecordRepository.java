7d19419280b34fe1bfe0c2ffc5da31e01d2bcecd
marrkapaa <151930230+marrkapaa@users.noreply.github.com>
2025-12-04 20:31:45 +0200

    Update VisitRecordRepository.java

--- src/main/java/gr/hua/dit/petcare/core/repositories/VisitRecordRepository.java
+++ src/main/java/gr/hua/dit/petcare/core/repositories/VisitRecordRepository.java
@@ -1,9 +1,14 @@
 package gr.hua.dit.petcare.core.repositories;
 
 import gr.hua.dit.petcare.core.model.VisitRecord;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface VisitRecordRepository extends JpaRepository<VisitRecord, Long> {
-    //
+    Optional<VisitRecord> findByAppointment_Id(Long appointmentId);
+
+    List<VisitRecord> findByAppointment_Pet_Id(Long petId);
 }
