6870f4ba08557732a53d03a282798ac8036a36a8
marrkapaa <151930230+marrkapaa@users.noreply.github.com>
2025-12-04 19:45:34 +0200

    Update AppointmentStatus.java

--- src/main/java/gr/hua/dit/petcare/core/model/AppointmentStatus.java
+++ src/main/java/gr/hua/dit/petcare/core/model/AppointmentStatus.java
@@ -1,9 +1,9 @@
 package gr.hua.dit.petcare.core.model;
 
 public enum AppointmentStatus {
-    PENDING,        // αναμένει επιβεβαίωση κτηνιάτρου
-    CONFIRMED,      // επιβεβαιωμένο ραντεβού
-    COMPLETED,      // ολοκληρωμένη επίσκεψη (μπορεί να έχει VisitRecord)
+    PENDING, // αναμένει επιβεβαίωση κτηνιάτρου
+    CONFIRMED, // επιβεβαιωμένο ραντεβού
+    COMPLETED, // ολοκληρωμένη επίσκεψη (μπορεί να έχει VisitRecord)
     CANCELLED_OWNER, // ακυρώθηκε από τον ιδιοκτήτη
-    CANCELLED_VET   // ακυρώθηκε από τον κτηνίατρο
+    CANCELLED_VET // ακυρώθηκε από τον κτηνίατρο
 }
